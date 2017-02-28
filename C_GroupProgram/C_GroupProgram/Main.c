743f3d8c8946dd6ea7b22d90adfc2fd876ebcfaf
MasayaNishijima <masaya20000607@gmail.com>
2017-02-28 15:39:00 +0900

    メインプログラム書き始め

--- C_GroupProgram/C_GroupProgram/Main.c
+++ C_GroupProgram/C_GroupProgram/Main.c
@@ -2,7 +2,13 @@
 #include <string.h>
 #include "Formula.h"
 
+int i;//‘I‘ğˆ‚ğ‘I‚Î‚¹‚é‚æ‚¤
+
 int main(void)
 {
-	
+	//OPNI();  //Å‰‚Ì‰æ–Ê
+	getchar();
+	//VSSE     //‘Îí‘Šè‚ğ‚Ç‚ê‚É‚·‚é‚©•\¦‚·‚é
+	scanf("%d", &i);
+
 }
